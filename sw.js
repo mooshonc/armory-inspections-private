9c9a288253e791dee02ad5c540e8e78a34cee21d
mooshonc <mooshonc@gmail.com>
2026-02-05 00:26:29 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,17 +1,31 @@
-// Service Worker removed - no longer in use
-// This file exists only to override any old cached version
-self.addEventListener('install', () => {
+// Service Worker - Self-destruct version
+// This file unregisters itself and clears all caches
+
+self.addEventListener('install', (event) => {
+  console.log('[SW] Installing cleanup version...');
   self.skipWaiting();
 });
 
 self.addEventListener('activate', (event) => {
+  console.log('[SW] Activating - cleaning up and unregistering...');
   event.waitUntil(
     caches.keys().then((cacheNames) => {
       return Promise.all(
-        cacheNames.map((cacheName) => caches.delete(cacheName))
+        cacheNames.map((cacheName) => {
+          console.log('[SW] Deleting cache:', cacheName);
+          return caches.delete(cacheName);
+        })
       );
     }).then(() => {
+      console.log('[SW] All caches deleted. Unregistering...');
       return self.registration.unregister();
+    }).then(() => {
+      console.log('[SW] Successfully unregistered');
     })
   );
+});
+
+// Don't intercept any requests
+self.addEventListener('fetch', (event) => {
+  return;
 });
